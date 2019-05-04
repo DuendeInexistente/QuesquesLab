7ecabab716780cd0efd18a169471e009412cdadc
Dude for realsies <DuendeInexistente@gmail.com>
2019-05-04 05:41:45 -0300

    Began sprites, dialogue prototyped, began using tilED.

---
+++ assets/backgrounds/Tilesets/First.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.04.25" name="First" tilewidth="8" tileheight="8" tilecount="1200" columns="24">
+ <image source="First.png" width="192" height="400"/>
+ <tile id="4" probability="0.25"/>
+ <tile id="5" probability="0.25"/>
+ <tile id="6" probability="3"/>
+ <tile id="28" probability="0.25"/>
+ <tile id="29" probability="0.25"/>
+ <tile id="52" probability="0.2"/>
+ <tile id="53" probability="0.2"/>
+ <tile id="76" probability="0.2"/>
+ <tile id="77" probability="0.2"/>
+ <tile id="80" probability="7"/>
+ <tile id="81" probability="3"/>
+ <tile id="101" probability="10"/>
+ <tile id="102" probability="10"/>
+ <tile id="388" probability="3"/>
+</tileset>
