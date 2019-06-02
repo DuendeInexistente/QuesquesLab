842622ebe04e0efec687924717a65e842b37a1ad
Dude for realsies <DuendeInexistente@gmail.com>
2019-06-02 04:43:28 -0300

    Updated after several days, added some new areas, refined the machien fix event, increased the spritesheet size again Committing as my hard drive fails

--- assets/backgrounds/Tilesets/First.tsx
+++ assets/backgrounds/Tilesets/First.tsx
@@ -1,26 +1,26 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2019.04.25" name="First" tilewidth="8" tileheight="8" tilecount="2400" columns="48">
- <image source="First.png" width="384" height="400"/>
+<tileset version="1.2" tiledversion="2019.05.08" name="First" tilewidth="8" tileheight="8" tilecount="5146" columns="83">
+ <image source="First.png" width="664" height="496"/>
  <tile id="4" probability="0.25"/>
  <tile id="5" probability="0.25"/>
  <tile id="6" probability="3"/>
- <tile id="52" probability="0.25"/>
- <tile id="53" probability="0.25"/>
- <tile id="100" probability="0.2"/>
- <tile id="101" probability="0.2"/>
- <tile id="148" probability="0.2"/>
- <tile id="149" probability="0.2"/>
- <tile id="152" probability="7"/>
- <tile id="153" probability="3"/>
- <tile id="197" probability="10"/>
- <tile id="198" probability="10"/>
- <tile id="586" probability="5"/>
- <tile id="587" probability="5"/>
- <tile id="588" probability="10"/>
- <tile id="589" probability="10"/>
- <tile id="634" probability="5"/>
- <tile id="635" probability="5"/>
- <tile id="636" probability="10"/>
- <tile id="637" probability="10"/>
- <tile id="772" probability="3"/>
+ <tile id="87" probability="0.25"/>
+ <tile id="88" probability="0.25"/>
+ <tile id="170" probability="0.2"/>
+ <tile id="171" probability="0.2"/>
+ <tile id="253" probability="0.2"/>
+ <tile id="254" probability="0.2"/>
+ <tile id="257" probability="7"/>
+ <tile id="258" probability="3"/>
+ <tile id="337" probability="10"/>
+ <tile id="338" probability="10"/>
+ <tile id="1006" probability="5"/>
+ <tile id="1007" probability="5"/>
+ <tile id="1008" probability="10"/>
+ <tile id="1009" probability="10"/>
+ <tile id="1089" probability="5"/>
+ <tile id="1090" probability="5"/>
+ <tile id="1091" probability="10"/>
+ <tile id="1092" probability="10"/>
+ <tile id="1332" probability="3"/>
 </tileset>
